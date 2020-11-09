d6f64b92eb74e5b931a4aca7e19b967b9a159a6c
Marta Noya <mnoyabon@redhat.com>
2020-11-09 17:47:42 +0100

    Optimize API endpoints for Tenant/Service proxy configs

---
+++ app/services/fetch_proxy_configs_service.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+class FetchProxyConfigsService
+  # TODO: too many params
+  def initialize(environment:, host: nil, version: nil, watcher: nil, owner:)
+    # watcher being the user/account who does the request (to check the permissions!)
+    # owner being either the service that owns the proxy configs or the tenant who does
+
+    @environment = environment
+    @host = host
+    @version = version
+    @watcher = watcher
+    @owner = owner
+  end
+
+  # TODO: maybe memoize because this is for the same params always (inside the same instance!)
+  def call
+    ProxyConfig
+      .where(proxy_id: proxy_ids)
+      .by_environment(environment)
+      .by_host(host)
+      .by_version(version)
+  end
+
+  private
+
+  attr_reader :environment, :host, :version, :watcher, :owner
+
+  def proxy_ids
+    Proxy.where(service_id: service_ids).pluck(:id)
+  end
+
+  def service_ids
+    accessible_services_ids & member_permission_service_ids
+  end
+
+  def accessible_services_ids
+    return [owner.id] if owner.kind_of?(Service)
+    return owner.accessible_services.pluck(:id) if owner.kind_of?(Account)
+    []
+  end
+
+  def member_permission_service_ids
+    ids = watcher.kind_of?(User) ? watcher.member_permission_service_ids : nil
+    ids || accessible_services_ids
+  end
+end
---
+++ test/unit/services/fetch_proxy_configs_service_test.rb
@@ -0,0 +1,113 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class FetchProxyConfigsServiceTest < ActiveSupport::TestCase
+  def setup
+    @provider = FactoryBot.create(:simple_provider)
+    @service = FactoryBot.create(:simple_service, :with_default_backend_api, account: provider)
+    @proxy = service.proxy
+  end
+
+  attr_reader :provider, :service, :proxy
+
+  test 'search by environment' do
+    ProxyConfig::ENVIRONMENTS.each { |env| FactoryBot.create_list(:proxy_config, 2, environment: env, proxy: proxy) }
+
+    ProxyConfig::ENVIRONMENTS.each do |env|
+      assert_same_elements proxy.proxy_configs.by_environment(env).select(:id).map(&:id), fetch_proxy_configs(environment: env).select(:id).map(&:id)
+    end
+
+    assert_raises(ProxyConfig::InvalidEnvironmentError) { fetch_proxy_configs(environment: 'invalid') }
+  end
+
+  test 'search by host' do
+    hosts_list = [
+      %w[3scale.localhost example.org],
+      %w[3scale.net example.org 3sca.net],
+      %w[3scale.localhost example.com]
+    ]
+
+    hosts_list.each do |hosts|
+      FactoryBot.create_list(:proxy_config, 2,
+                                proxy: proxy,
+                                environment: ProxyConfig::ENVIRONMENTS.first,
+                                content: { proxy: { hosts: hosts } }.to_json
+      )
+    end
+
+    proxy_configs_by_example_org_host = proxy.proxy_configs.by_host('example.org').by_environment(ProxyConfig::ENVIRONMENTS.first).select(:id).map(&:id)
+    assert_same_elements proxy_configs_by_example_org_host, fetch_proxy_configs(host: 'example.org').select(:id).map(&:id)
+
+    proxy_configs_for_all_hosts = proxy.proxy_configs.by_environment(ProxyConfig::ENVIRONMENTS.first).select(:id).map(&:id)
+    assert_same_elements proxy_configs_for_all_hosts, fetch_proxy_configs(host: nil).select(:id).map(&:id)
+  end
+
+  test 'search by version' do
+    FactoryBot.create_list(:proxy_config, 3,
+                           proxy: proxy,
+                           environment: ProxyConfig::ENVIRONMENTS.first
+    )
+
+    proxy_configs_by_version_2 = proxy.proxy_configs.by_version(2).by_environment(ProxyConfig::ENVIRONMENTS.first).select(:id).map(&:id)
+    assert_same_elements proxy_configs_by_version_2, fetch_proxy_configs(version: 2).select(:id).map(&:id)
+    assert_same_elements proxy_configs_by_version_2, fetch_proxy_configs(version: '2').select(:id).map(&:id)
+
+    proxy_configs_by_latest_version = proxy.proxy_configs.by_version('latest').by_environment(ProxyConfig::ENVIRONMENTS.first).select(:id, :version).map(&:id)
+    assert_same_elements proxy_configs_by_latest_version, fetch_proxy_configs(version: 'latest').select(:id, :version).map(&:id)
+
+    proxy_configs_for_all_versions = proxy.proxy_configs.by_environment(ProxyConfig::ENVIRONMENTS.first).select(:id).map(&:id)
+    assert_same_elements proxy_configs_for_all_versions, fetch_proxy_configs(version: nil).select(:id).map(&:id)
+  end
+
+  test 'search by owner: service or provider' do
+    another_service = FactoryBot.create(:simple_service, :with_default_backend_api, account: provider)
+    service_of_another_provider = FactoryBot.create(:simple_service, :with_default_backend_api)
+    [service, another_service, service_of_another_provider].each do |service|
+      FactoryBot.create_list(:proxy_config, 2, proxy: service.proxy, environment: ProxyConfig::ENVIRONMENTS.first)
+    end
+
+    proxy_configs_of_provider = ProxyConfig.for_services(provider.services).by_environment(ProxyConfig::ENVIRONMENTS.first).select(:id).map(&:id)
+    assert_same_elements proxy_configs_of_provider, fetch_proxy_configs(owner: provider).select(:id).map(&:id)
+
+    proxy_configs_of_service = service.proxy.proxy_configs.where(proxy_id: service.proxy.id).by_environment(ProxyConfig::ENVIRONMENTS.first).select(:id).map(&:id)
+    assert_same_elements proxy_configs_of_service, fetch_proxy_configs(owner: service).select(:id).map(&:id)
+  end
+
+  test 'search filters by permissions' do
+    skip 'TODO WIP'
+    services = [service] | FactoryBot.create_list(:simple_service, 2, :with_default_backend_api, account: provider)
+    services.each { |service| FactoryBot.create(:proxy_config, proxy: service.proxy, environment: ProxyConfig::ENVIRONMENTS.first) }
+
+    admin = FactoryBot.create(:admin, account: provider)
+    member_access_all_services = FactoryBot.create(:member, account: provider, admin_sections: ['partners'])
+    member_access_all_services.update!(member_permission_service_ids: nil)
+    member_access_some_services = FactoryBot.create(:member, account: provider, admin_sections: ['partners'])
+    member_access_some_services.update!(member_permission_service_ids: services[0..1].map(&:id))
+    member_with_sections_permissions_but_empty_services = FactoryBot.create(:member, account: provider, admin_sections: ['partners'])
+    member_with_sections_permissions_but_empty_services.update!(member_permission_service_ids: '[]')
+    member_without_sections_permissions_but_with_services_ids = FactoryBot.create(:member, account: provider, admin_sections: [])
+    member_without_sections_permissions_but_with_services_ids.update!(member_permission_service_ids: services[0..1].map(&:id))
+
+    services.each { |s| pp s.proxy.proxy_configs.select(:id).map(&:id) }
+    pp "-------------------------------"
+    [admin, member_access_all_services, member_access_some_services, member_with_sections_permissions_but_empty_services, member_without_sections_permissions_but_with_services_ids].each do |user|
+      pp fetch_proxy_configs(owner: provider, watcher: user).select(:id).map(&:id)
+    end
+  end
+
+  test 'search filters accessible services only' do
+    deleted_service = FactoryBot.create(:simple_service, :with_default_backend_api, account: provider)
+    deleted_service.mark_as_deleted!
+    [service, deleted_service].each { |service| FactoryBot.create(:proxy_config, proxy: service.proxy, environment: ProxyConfig::ENVIRONMENTS.first) }
+
+    expected_non_deleted_proxy_configs = service.proxy.proxy_configs.select(:id).map(&:id)
+    assert_equal expected_non_deleted_proxy_configs, fetch_proxy_configs(owner: provider).select(:id).map(&:id)
+  end
+
+  private
+
+  def fetch_proxy_configs(environment: ProxyConfig::ENVIRONMENTS.first, owner: service, **options)
+    FetchProxyConfigsService.new(environment: environment, owner: owner, **options).call
+  end
+end
